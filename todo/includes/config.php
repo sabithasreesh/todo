bd70561c34389cb0232c9a2f69a911dd334f0f31
sabithasreesh <sabith22@gmail.com>
2016-11-02 04:32:50 +0100

    Added Comments

--- todo/includes/config.php
+++ todo/includes/config.php
@@ -5,7 +5,9 @@
  * configuartion variables are defined here
  * @author Sabitha 
  */
+// url of the rest api
 $api_url = "http://localhost:8088/restapi/api/";
 
+// Base url of the client application
 $base_url = "http://localhost:8088/todo/";
 
